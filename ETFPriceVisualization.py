393945df1fd3230c9f3aefcd7ee5b1fde4e905cb
Ornello <iannaceornello@gmail.com>
2020-12-29 13:59:57 -0500

    Added volume vs date graph

--- ETFPriceVisualization.py
+++ ETFPriceVisualization.py
@@ -15,22 +15,24 @@ class ETF:
         self.ticker = ticker
         self.URL = URL
         self.Dates = []
+        self.Dates2 = []
         self.Prices = []
-        
+        self.OpenPrices = []
+        self.tradingvolume = []
     def getcurrentprice(self):
         page = requests.get(self.URL)
         soup = BeautifulSoup(page.content, 'html.parser')
         Current = soup.find('span', attrs={"data-reactid":"32"})
         return Current.text
-    def returnssinceOct2017(self):
-        return (self.Prices[-1]/self.Prices[1])
+    def createDates(self, filename, datetype , datecol):
+        tempDates = np.genfromtxt(filename, skip_header = 1, delimiter = ',', usecols = datecol, dtype = datetype)
+        self.Dates = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in tempDates]
     #def holdingsdistributiongraph(self):                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
     def sharepricegraph(self, filename, datetype, pricetype, datecol, pricecol, ma):
-        self.Dates = np.genfromtxt(filename, skip_header = 1, delimiter = ',', usecols = datecol, dtype = datetype)
+        self.createDates(filename,  datetype, datecol)
         self.Prices = np.genfromtxt(filename, skip_header = 1, delimiter = ',', usecols = pricecol, dtype = pricetype)
 
-        Datesformat = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in self.Dates]
-        CompleteTable = np.column_stack((Datesformat, self.Prices))
+        CompleteTable = np.column_stack((self.Dates, self.Prices))
         
         fig = plt.figure(1)  
         plt.ion()
@@ -41,7 +43,7 @@ class ETF:
 
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
         plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = 75))
-        plt.plot(Datesformat, self.Prices, label = self.ticker)
+        plt.plot(self.Dates, self.Prices, label = self.ticker)
         
         plt.legend(bbox_to_anchor=(0.9, 0.33), loc='upper left', borderaxespad=0.)
         
@@ -50,23 +52,29 @@ class ETF:
         plot_widget.grid(row = 0, column = 1)
             
         plt.gcf().autofmt_xdate()
+    def VolvsOpenPrice(self, filename, datetype, openpricetype, voltype, datecol, openpricecol, volcol, multiplier, ma):
+        self.createDates(filename, datetype, datecol)
+        self.tradingvolume = np.genfromtxt(filename, skip_header = 1, delimiter = ',', usecols = volcol, dtype = voltype)
+        self.OpenPrices = np.genfromtxt(filename, skip_header = 1, delimiter = ',', usecols = openpricecol, dtype = openpricetype)
+        for i in range(len(self.tradingvolume)):
+            self.tradingvolume[i] = self.tradingvolume[i]*multiplier
+        CompleteTable = np.column_stack((self.Dates, self.tradingvolume))
         
-# m = tkinter.Tk() 
-     
-# VEETO = ETF("VEE.TO", 'https://ca.finance.yahoo.com/quote/VEE.TO/history?p=VEE.TO&.tsrc=fin-srch')
-# VIUTO = ETF("VIU.TO", "https://ca.finance.yahoo.com/quote/VIU.TO/")
-# XICTO = ETF("XIC.TO", "https://ca.finance.yahoo.com/quote/XIC.TO?p=XIC.TO&.tsrc=fin-srch")
-# XUUTO = ETF("XUU.T0", "https://ca.finance.yahoo.com/quote/XUU.TO?p=XUU.TO&.tsrc=fin-srch")
-
-# Currentprices = "CURRENT PRICES\n\n" + VEETO.ticker + " " + VEETO.getcurrentprice() +"\n"+ VIUTO.ticker + " " + VIUTO.getcurrentprice()+ "\n" + XICTO.ticker + " " + XICTO.getcurrentprice()+ "\n" + XUUTO.ticker + " " + XUUTO.getcurrentprice()+"\n",datetime.now()
-# Currentpricevar = Message(m, text = Currentprices)
-# Currentpricevar.config(width = 110)
-# Currentpricevar.grid(row = 0, column = 0)
-
-# VEETO.sharepricegraph("VEE.TO.csv", str, float, 0, 4,m)
-# VIUTO.sharepricegraph("VIU.TO.csv", str, float, 0, 4,m)
-# XICTO.sharepricegraph("XIC.TO.csv", str, float, 0, 4,m)
-# XUUTO.sharepricegraph("XUU.TO.csv", str, float, 0, 4,m)
-
-# #print(VEETO.returnssince2017())
-# m.mainloop()
+        fig = plt.figure(2)  
+        plt.ion()
+        
+        plt.xlabel('Date')
+        plt.ylabel('Trading Volume')
+        plt.title("Trading Volume (2017-2020)")
+        
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = 75))
+        plt.plot(self.Dates, self.tradingvolume, label = self.ticker + "*" + str(multiplier))
+        
+        plt.legend(bbox_to_anchor=(0.07, 0.9), loc='upper left', borderaxespad=0.)
+        
+        canvas = FigureCanvasTkAgg(fig,master = ma)
+        plot_widget = canvas.get_tk_widget()
+        plot_widget.grid(row = 1, column = 1)
+        
+        plt.gcf().autofmt_xdate()
--- VisualizationControl.py
+++ VisualizationControl.py
@@ -16,15 +16,19 @@ VIUTO = ETFPriceVisualization.ETF("VIU.TO", "https://ca.finance.yahoo.com/quote/
 XICTO = ETFPriceVisualization.ETF("XIC.TO", "https://ca.finance.yahoo.com/quote/XIC.TO?p=XIC.TO&.tsrc=fin-srch")
 XUUTO = ETFPriceVisualization.ETF("XUU.T0", "https://ca.finance.yahoo.com/quote/XUU.TO?p=XUU.TO&.tsrc=fin-srch")
 
-Currentprices = "CURRENT PRICES\n\n" + VEETO.ticker + " " + VEETO.getcurrentprice() +"\n"+ VIUTO.ticker + " " + VIUTO.getcurrentprice()+ "\n" + XICTO.ticker + " " + XICTO.getcurrentprice()+ "\n" + XUUTO.ticker + " " + XUUTO.getcurrentprice()+"\n",datetime.now()
-Currentpricevar = Message(m, text = Currentprices)
-Currentpricevar.config(width = 110)
-Currentpricevar.grid(row = 0, column = 0)
 
 VEETO.sharepricegraph("VEE.TO.csv", str, float, 0, 4,m)
 VIUTO.sharepricegraph("VIU.TO.csv", str, float, 0, 4,m)
 XICTO.sharepricegraph("XIC.TO.csv", str, float, 0, 4,m)
 XUUTO.sharepricegraph("XUU.TO.csv", str, float, 0, 4,m)
 
+VEETO.VolvsOpenPrice("VEE.TO.csv", str, float, int, 0, 1, 6, 10, m)
+VIUTO.VolvsOpenPrice("VIU.TO.csv", str, float, int, 0, 1, 6, 10, m)
+XICTO.VolvsOpenPrice("XIC.TO.csv", str, float, int, 0, 1, 6, 1, m)
+XUUTO.VolvsOpenPrice("XUU.TO.csv", str, float, int, 0, 1, 6, 10, m)
+Overview = "CURRENT PRICES\n\n" + VEETO.ticker + " " + VEETO.getcurrentprice() +"\n" + VIUTO.ticker + " " + VIUTO.getcurrentprice()+ "\n" + XICTO.ticker + " " + XICTO.getcurrentprice()+ "\n" + XUUTO.ticker + " " + XUUTO.getcurrentprice()+"\n",datetime.now()
+Currentpricevar = Message(m, text = Overview)
+Currentpricevar.config(width = 110)
+Currentpricevar.grid(row = 0, column = 0)
 #print(VEETO.returnssince2017())
 m.mainloop()
